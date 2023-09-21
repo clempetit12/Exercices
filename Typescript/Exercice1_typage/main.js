167a096a333fa8b87b5ce85ff77175e7f7cea761
Clémence <clempetit12@gmail.com>
2023-09-21 16:26:49 +0200

    Exercice typage

---
+++ Typescript/Exercice1_typage/main.js
@@ -0,0 +1,27 @@
+"use strict";
+let userName = "Clémence";
+let userAge = 30;
+let isLogin = true;
+let userNames = ["toto", "tata"];
+userNames.push(userName);
+console.log(userNames);
+let person;
+person = {
+    firstName: "Dupont",
+    age: 20,
+    isLoggedIn: true
+};
+console.log(person.age);
+let person1 = {
+    firstName: "Dupont",
+    age: 20,
+    isLoggedIn: true
+};
+console.log(person1.age);
+let person2 = {
+    firstName: "Dupont",
+    age: 20,
+    isLoggedIn: true
+};
+console.log(person2.age);
+let infos;
---
+++ Typescript/Exercice1_typage/main.ts
@@ -0,0 +1,38 @@
+let userName: string = "Clémence";
+let userAge: number = 30;
+let isLogin: boolean = true;
+let userNames = ["toto", "tata"]
+userNames.push(userName)
+
+console.log(userNames);
+
+let person: object
+
+person = {
+    firstName: "Dupont",
+    age: 20,
+    isLoggedIn: true
+}
+console.log(person.age);
+
+let person1: {
+    firstName: string,
+    age: number,
+    isLoggedIn: boolean
+} = {
+    firstName: "Dupont",
+    age: 20,
+    isLoggedIn: true
+}
+console.log(person1.age);
+
+let person2 = {
+    firstName: "Dupont",
+     age: 20,
+      isLoggedIn: true
+}
+
+console.log(person2.age);
+
+let infos : [string, number]
+

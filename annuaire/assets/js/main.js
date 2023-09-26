eaaca37bdccc66596de01d167e1d17d6a6cdcfc5
clempetit12 <143411906+clempetit12@users.noreply.github.com>
2023-09-26 13:33:20 +0200

    TP Annuaire VF

--- annuaire/assets/js/main.js
+++ annuaire/assets/js/main.js
@@ -6,37 +6,46 @@ let dateOfBirth = document.getElementById("dateOfBirth");
 let email = document.getElementById("email");
 let phoneNumber = document.getElementById("phoneNumber");
 let buttonContact = document.querySelectorAll(".buttonContact");
+let buttonAdd = document.getElementById("openModal");
+let modal = document.getElementById("modal");
+let closeModal = document.getElementById("closeModal");
+let editButton = document.getElementById("edit");
+let deleteButton = document.getElementById("delete");
+let inputElements = document.querySelectorAll("input");
+let formulaire = document.getElementById("formulaire");
+let save = document.getElementById("save");
+let add = document.getElementById("add");
+let firstnameModal = document.getElementById("firstnameModal");
+let lastnameModal = document.getElementById("lastnameModal");
+let dateOfBirthModal = document.getElementById("dateOfBirthModal");
+let emailModal = document.getElementById("emailModal");
+let phoneNumberModal = document.getElementById("phoneNumberModal");
+let contactAdd = document.getElementById("contactAdd");
 let person1;
 let person2;
 let person3;
 let person4;
 person1 = new Person("Albert", "DUPONT", "05/05/1992", "albertdupont@gmail.com", "+(33)0645125859");
-person2 = new Person("John", "SMITH", "12/05/1993", "albertdupont@gmail.com", "+(33)0645125859");
-person3 = new Person("Clara", "GOMEZ", "12/05/1993", "albertdupont@gmail.com", "+(33)0645125859");
-person4 = new Person("Elizabeth", "MARTIN", "12/05/1993", "albertdupont@gmail.com", "+(33)0645125859");
+person2 = new Person("John", "SMITH", "12/05/1993", "johnsmith@gmail.com", "+(33)0645125889");
+person3 = new Person("Clara", "GOMEZ", "12/12/1993", "claragomez@gmail.com", "+(33)064512589");
+person4 = new Person("Elizabeth", "MARTIN", "12/07/1993", "elizabethmartin@gmail.com", "+(33)0655125859");
 let contactsTab = [];
+// Je crée un tableau de Contacts pour ajouter mes personnes prééxistantes
 contactsTab.push(person1, person2, person3, person4);
 console.table(contactsTab);
+// Je charge mon DOM en créant pour chaque élément du tableau un bouton avec le nom & prénom
 document.addEventListener('DOMContentLoaded', function () {
     console.log("bonjour");
     contactsTab.forEach(element => {
-        let button = document.createElement('button');
-        button.type = 'button';
-        button.id = `${element.id}`;
-        button.innerHTML = `${element.firstname} ${element.lastname}`;
-        button.className = `row btn btn-outline-light btn-lg btn-block buttonContact `;
-        contacts.appendChild(button);
+        createBouton(element);
     });
-    let buttonAdd = document.createElement('button');
-    buttonAdd.type = 'button';
-    buttonAdd.innerHTML = `Add`;
-    buttonAdd.className = "row btn btn-outline-light btn-lg btn-block";
-    console.log(buttonContact);
     buttonContact = document.querySelectorAll(".buttonContact");
     console.log(buttonContact);
     chargerBouton();
 });
+// fonction pour charger les boutons créés et lui associer une fonctionnalité au bouton crée pour afficher contact dans la page à côté 
 function chargerBouton() {
+    console.log("charger bouton");
     buttonContact.forEach(function (bouton) {
         console.log(`le bouton `);
         bouton.addEventListener("click", function () {
@@ -47,6 +56,7 @@ function chargerBouton() {
         });
     });
 }
+//fonction pour afficher les éléments de la personne trouvée dans le tableau dans l'encart Détails
 function display(personneTrouvee) {
     console.log("display");
     firstname.value = personneTrouvee.firstname;
@@ -56,6 +66,7 @@ function display(personneTrouvee) {
     email.value = personneTrouvee.email;
     phoneNumber.value = separationPhoneNumber(personneTrouvee.phoneNumber);
 }
+// Fonction pour afficher le numéro de téléphone avec le bon format
 function separationPhoneNumber(numero) {
     const segment1 = numero.slice(0, 5);
     const segment2 = numero.slice(5, 7);
@@ -65,4 +76,72 @@ function separationPhoneNumber(numero) {
     const segment6 = numero.slice(13, 15);
     const numeroFormate = `${segment1} ${segment2} ${segment3} ${segment4} ${segment5} ${segment6}`;
     return numeroFormate;
+}
+// Essayer d'afficher la modale mais ça ne fonctionne pas 
+buttonAdd.addEventListener('click', () => {
+    modal.style.display = "block";
+});
+closeModal.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
+//editer bouton mais ça ne fonctionne pas 
+editButton.addEventListener('click', () => {
+    console.log("bonjour edit");
+    let personneModifieelastname = lastname.value;
+    console.log(personneModifieelastname);
+    chercherIdParNom(personneModifieelastname);
+    console.log(chercherIdParNom(personneModifieelastname));
+    editButton.style.display = "none";
+    save.style.display = "block";
+});
+formulaire.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const nouveauPrenom = firstname.value;
+    const nouveauNom = lastname.value;
+    console.table(contactsTab);
+    updateTab();
+    editButton.style.display = "block";
+    save.style.display = "none";
+});
+function updateTab() {
+    const nouveauPrenom = firstname.value;
+    const nouveauNom = lastname.value;
+    let personneModifiee = `${nouveauNom}${nouveauPrenom}`;
+    console.log(personneModifiee);
+}
+function chercherIdParNom(personnecherchee) {
+    for (let i = 0; i < contactsTab.length; i++) {
+        if (contactsTab[i].lastname === personnecherchee) {
+            return contactsTab[i].id;
+        }
+        return null;
+    }
+}
+// Ajouter un nouveau contact à la liste des contacts
+add.addEventListener('click', () => {
+    let firstname = firstnameModal.value;
+    console.log(firstname);
+    let lastname = lastnameModal.value;
+    let dateOfBirth = dateOfBirthModal.value;
+    let email = emailModal.value;
+    let phoneNumber = phoneNumberModal.value;
+    let personAdd = new Person(firstname, lastname, dateOfBirth, email, phoneNumber);
+    contactsTab.push(personAdd);
+    console.table(contactsTab);
+    createBouton(personAdd);
+    chargerBouton();
+});
+// Fonction pour créer un bouton
+function createBouton(element) {
+    let button = document.createElement('button');
+    button.type = 'button';
+    button.id = `${element.id}`;
+    button.innerHTML = `${element.firstname} ${element.lastname}`;
+    button.className = `row btn btn-outline-light btn-lg btn-block buttonContact `;
+    contacts.appendChild(button);
 }
--- annuaire/assets/ts/main.ts
+++ annuaire/assets/ts/main.ts
@@ -7,7 +7,21 @@ let dateOfBirth = document.getElementById("dateOfBirth") as HTMLInputElement
 let email = document.getElementById("email") as HTMLInputElement
 let phoneNumber = document.getElementById("phoneNumber") as HTMLInputElement
 let buttonContact = document.querySelectorAll(".buttonContact") as NodeListOf<HTMLElement>
-
+let buttonAdd = document.getElementById("openModal") as HTMLElement
+let modal = document.getElementById("modal") as HTMLElement
+let closeModal = document.getElementById("closeModal") as HTMLElement
+let editButton = document.getElementById("edit") as HTMLElement
+let deleteButton = document.getElementById("delete") as HTMLElement
+let inputElements = document.querySelectorAll("input") 
+let formulaire = document.getElementById("formulaire")
+let save = document.getElementById("save")
+let add = document.getElementById("add")
+let firstnameModal = document.getElementById("firstnameModal") as HTMLInputElement
+let lastnameModal = document.getElementById("lastnameModal") as HTMLInputElement
+let dateOfBirthModal = document.getElementById("dateOfBirthModal") as HTMLInputElement
+let emailModal = document.getElementById("emailModal") as HTMLInputElement
+let phoneNumberModal = document.getElementById("phoneNumberModal") as HTMLInputElement
+let contactAdd = document.getElementById("contactAdd")
 
 
 let person1:Person
@@ -16,40 +30,32 @@ let person3:Person
 let person4:Person
 
  person1 = new Person ("Albert", "DUPONT","05/05/1992", "albertdupont@gmail.com", "+(33)0645125859")
- person2 = new Person ("John", "SMITH", "12/05/1993", "albertdupont@gmail.com", "+(33)0645125859")
- person3 = new Person ("Clara", "GOMEZ", "12/05/1993", "albertdupont@gmail.com", "+(33)0645125859")
- person4 = new Person ("Elizabeth", "MARTIN", "12/05/1993", "albertdupont@gmail.com", "+(33)0645125859")
+ person2 = new Person ("John", "SMITH", "12/05/1993", "johnsmith@gmail.com", "+(33)0645125889")
+ person3 = new Person ("Clara", "GOMEZ", "12/12/1993", "claragomez@gmail.com", "+(33)064512589")
+ person4 = new Person ("Elizabeth", "MARTIN", "12/07/1993", "elizabethmartin@gmail.com", "+(33)0655125859")
 
 let contactsTab: Person[] = [];
 
-
+// Je crée un tableau de Contacts pour ajouter mes personnes prééxistantes
 contactsTab.push(person1,person2,person3,person4)
 console.table(contactsTab);
 
+// Je charge mon DOM en créant pour chaque élément du tableau un bouton avec le nom & prénom
 document.addEventListener('DOMContentLoaded', function(){
     console.log("bonjour");
-    contactsTab.forEach(element => {
-        let button = document.createElement('button')
-        button.type = 'button'
-        button.id = `${element.id}`
-        button.innerHTML = `${element.firstname} ${element.lastname}`
-        button.className = `row btn btn-outline-light btn-lg btn-block buttonContact `
-        contacts.appendChild(button)    
+   contactsTab.forEach(element => {
+    createBouton(element)   
     });
-    let buttonAdd = document.createElement('button')
-    buttonAdd.type = 'button'
-    buttonAdd.innerHTML = `Add`
-    buttonAdd.className = "row btn btn-outline-light btn-lg btn-block"
-    console.log(buttonContact);
     buttonContact = document.querySelectorAll(".buttonContact") as NodeListOf<HTMLElement>
-    console.log(buttonContact);
+    console.log(buttonContact);  
     chargerBouton()
     
 })
 
+// fonction pour charger les boutons créés et lui associer une fonctionnalité au bouton crée pour afficher contact dans la page à côté 
 function chargerBouton (){
+    console.log("charger bouton");
 buttonContact.forEach(function(bouton) {
-
     console.log(`le bouton `);
     bouton.addEventListener("click", function() {
         console.log("selecteur");
@@ -60,8 +66,8 @@ buttonContact.forEach(function(bouton) {
        });
     });}
 
-
-function display (personneTrouvee) {
+//fonction pour afficher les éléments de la personne trouvée dans le tableau dans l'encart Détails
+function display (personneTrouvee: Person) {
     console.log("display");
     firstname.value = personneTrouvee.firstname
     lastname.value = personneTrouvee.lastname
@@ -69,11 +75,9 @@ function display (personneTrouvee) {
     dateOfBirth.value = date
     email.value = personneTrouvee.email
     phoneNumber.value = separationPhoneNumber(personneTrouvee.phoneNumber)
-
-
-
 }
 
+// Fonction pour afficher le numéro de téléphone avec le bon format
 function separationPhoneNumber (numero) {
     const segment1 = numero.slice(0, 5);
     const segment2 = numero.slice(5, 7);
@@ -83,4 +87,85 @@ function separationPhoneNumber (numero) {
     const segment6 = numero.slice(13, 15);
     const numeroFormate = `${segment1} ${segment2} ${segment3} ${segment4} ${segment5} ${segment6}`;
     return numeroFormate
+}
+
+// Essayer d'afficher la modale mais ça ne fonctionne pas 
+buttonAdd.addEventListener('click', ()=> {
+    modal.style.display = "block"
+
+})
+closeModal.addEventListener("click", function() {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+
+  //editer bouton mais ça ne fonctionne pas 
+  editButton.addEventListener('click', () => {
+    console.log("bonjour edit");
+    let personneModifieelastname =  lastname.value
+    console.log(personneModifieelastname);
+    chercherIdParNom(personneModifieelastname)
+    console.log(chercherIdParNom(personneModifieelastname));
+    editButton.style.display = "none";
+    save.style.display = "block";
+  })
+    
+    formulaire.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+        const nouveauPrenom = firstname.value;
+        const nouveauNom = lastname.value;
+        console.table(contactsTab);
+        updateTab()
+       
+        editButton.style.display = "block";
+        save.style.display = "none";
+    })
+
+function updateTab () {
+    const nouveauPrenom = firstname.value;
+        const nouveauNom = lastname.value;
+    let personneModifiee =`${nouveauNom}${nouveauPrenom}`
+    console.log(personneModifiee);
+        
+       }
+   
+
+       function chercherIdParNom(personnecherchee) {
+        for (let i = 0; i < contactsTab.length; i++) {
+            if (contactsTab[i].lastname === personnecherchee) {
+                return contactsTab[i].id; 
+            }
+            return null; 
+        }
+        
+    }
+
+    // Ajouter un nouveau contact à la liste des contacts
+    add.addEventListener('click', () => {
+        let firstname = firstnameModal.value
+        console.log(firstname);
+        let lastname = lastnameModal.value
+        let dateOfBirth = dateOfBirthModal.value
+        let email = emailModal.value
+        let phoneNumber = phoneNumberModal.value      
+let personAdd = new Person (firstname, lastname, dateOfBirth, email, phoneNumber)
+contactsTab.push(personAdd)
+console.table(contactsTab);
+createBouton(personAdd)
+chargerBouton()
+    })
+
+// Fonction pour créer un bouton
+function createBouton (element) {
+    let button = document.createElement('button')
+button.type = 'button'
+button.id = `${element.id}`
+button.innerHTML = `${element.firstname} ${element.lastname}`
+button.className = `row btn btn-outline-light btn-lg btn-block buttonContact `
+contacts.appendChild(button)   
 }

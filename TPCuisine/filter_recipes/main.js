f0c1cd6821ac5dcc871a32fdb9f23bff50f706f2
clempetit12 <143411906+clempetit12@users.noreply.github.com>
2023-09-28 13:43:42 +0200

    TP cuisine V1

--- TPCuisine/filter_recipes/main.js
+++ TPCuisine/filter_recipes/main.js
@@ -2,7 +2,7 @@ import { recipes } from "./assets/ts/data/recipes.js";
 let preparationTime = document.getElementById("preparationTime");
 let cookingTime = document.getElementById("cookingTime");
 let search = document.getElementById("search");
-let ingredients = document.getElementById("ingredients");
+let ingredientsIndex = document.getElementById("ingredients");
 let filter = document.getElementById("filter");
 let recettes = document.getElementById("recettes");
 let tabRecipes = [];
@@ -12,15 +12,33 @@ for (const key in recipes) {
         tabRecipes.push(element);
     }
 }
-recettes.innerHTML;
+const ingredient = tabRecipes.map(el => el.ingredients);
+console.log(ingredient[0][0].name);
+/*     console.log(tabRecipes[0].name);
 tabRecipes.forEach(element => {
+    for (let i=0; i<ingredients.length; i++) {
+        console.log(`${element.ingredients[i]}`);
+            }
+}); */
+let i = 0;
+window.addEventListener('load', function () {
     tabRecipes.forEach(element => {
-        console.log(tabRecipes[name]);
+        createBouton(element);
+        chargerListeIngredients(element);
     });
 });
-function affichage(cle) {
+function createBouton(element) {
     let button = document.createElement('button');
-    button.textContent =
-    ;
+    button.type = 'button';
+    button.innerHTML = `${element.name} <hr>`;
+    button.innerHTML += `<i class="bi bi-egg-fried"></i>${element.cookTime}  <i class="bi bi-fire"></i>${element.prepTime} `;
+    button.className = "btn btn-outline-light btn-lg btn-block text-center";
+    recettes.appendChild(button);
+}
+function chargerListeIngredients(element) {
+    let option = document.createElement('option');
+    option.innerHTML = `${ingredient[0][0].name}`;
+    ingredientsIndex.appendChild(option);
 }
 console.table(tabRecipes);
+console.log(tabRecipes[0].name);
--- TPCuisine/filter_recipes/main.ts
+++ TPCuisine/filter_recipes/main.ts
@@ -4,7 +4,7 @@ import { recipes } from "./assets/ts/data/recipes.js"
 let preparationTime = document.getElementById("preparationTime") as HTMLLabelElement
 let cookingTime = document.getElementById("cookingTime") as HTMLLabelElement
 let search = document.getElementById("search") as HTMLDivElement
-let ingredients = document.getElementById("ingredients") as HTMLSelectElement
+let ingredientsIndex = document.getElementById("ingredients") as HTMLSelectElement
 let filter = document.getElementById("filter") as HTMLButtonElement
 let recettes = document.getElementById("recettes") as HTMLDivElement
 
@@ -35,20 +35,57 @@ let tabRecipes: Recipe[] = []
           
         }
     }
+  
+    const ingredient = tabRecipes.map(el => el.ingredients)
+  
+    console.log(ingredient[0][0].name);
 
-recettes.innerHTML
-
+/*     console.log(tabRecipes[0].name);
 tabRecipes.forEach(element => {
+    for (let i=0; i<ingredients.length; i++) {
+        console.log(`${element.ingredients[i]}`);
+            }
+}); */
 
-tabRecipes.forEach(element => {
-    console.log(tabRecipes[name]);
-    
-});
-});
+       let i =0
+            window.addEventListener('load', function() {
+                
+                tabRecipes.forEach(element => {
+                  createBouton(element)
+               
+                  chargerListeIngredients(element)
 
- function affichage (cle: string) {
-    let button = document.createElement('button')
-    button.textContent = 
+                });
+            })
+
+
+    function createBouton (element: Recipe) {
+        let button = document.createElement('button')
+        button.type = 'button';
+        button.innerHTML=`${element.name} <hr>` 
+        button.innerHTML +=`<i class="bi bi-egg-fried"></i>${element.cookTime}  <i class="bi bi-fire"></i>${element.prepTime} ` 
+        button.className = "btn btn-outline-light btn-lg btn-block text-center"
+        recettes.appendChild(button);
+
+        
+    }
+function chargerListeIngredients (element: Recipe ) {
+let option = document.createElement('option')
+option.innerHTML = `${ingredient[0][0].name }` 
+ingredientsIndex.appendChild(option)
 }
+    
+
+  
+
+
+   
+   
+   
+
+
  
+
+
 console.table(tabRecipes);
+console.log(tabRecipes[0].name);

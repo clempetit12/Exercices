d8c8191847bcdc5b9951eb8e93d1ef767ab5f5cf
clempetit12 <143411906+clempetit12@users.noreply.github.com>
2023-09-28 22:35:16 +0200

    TP Cuisine version intermediaire

--- TPCuisine/filter_recipes/main.js
+++ TPCuisine/filter_recipes/main.js
@@ -5,40 +5,60 @@ let search = document.getElementById("search");
 let ingredientsIndex = document.getElementById("ingredients");
 let filter = document.getElementById("filter");
 let recettes = document.getElementById("recettes");
+let modal = document.getElementById("modal");
+let titre = document.getElementById("titre");
+let ingredientModal = document.getElementById("ingredientModal");
+let instructionModal = document.getElementById("instruction");
 let tabRecipes = [];
+let selectedRecipe = tabRecipes[0];
 for (const key in recipes) {
     if (recipes.hasOwnProperty.call(recipes, key)) {
         const element = recipes[key];
         tabRecipes.push(element);
     }
 }
-const ingredient = tabRecipes.map(el => el.ingredients);
-console.log(ingredient[0][0].name);
-/*     console.log(tabRecipes[0].name);
-tabRecipes.forEach(element => {
-    for (let i=0; i<ingredients.length; i++) {
-        console.log(`${element.ingredients[i]}`);
-            }
-}); */
-let i = 0;
+console.table(tabRecipes);
 window.addEventListener('load', function () {
+    console.log("object");
     tabRecipes.forEach(element => {
         createBouton(element);
-        chargerListeIngredients(element);
     });
 });
 function createBouton(element) {
     let button = document.createElement('button');
     button.type = 'button';
     button.innerHTML = `${element.name} <hr>`;
+    button.className = element === selectedRecipe ? "btn btn-light w-100 my-2" : "btn btn-outline-light w-100 my-2";
     button.innerHTML += `<i class="bi bi-egg-fried"></i>${element.cookTime}  <i class="bi bi-fire"></i>${element.prepTime} `;
-    button.className = "btn btn-outline-light btn-lg btn-block text-center";
+    button.addEventListener('click', () => {
+        selectedRecipe = element;
+        console.log(selectedRecipe);
+        modal.style.display = "block";
+        titre.innerHTML = element.name;
+        instructionModal.innerHTML = `${element.instructions}`;
+    });
     recettes.appendChild(button);
+    button.addEventListener('click', () => {
+        selectedRecipe = element;
+        console.log(selectedRecipe);
+        tabRecipes.forEach(element => {
+            const ingredientsList = element.ingredients;
+            console.log(ingredientsList.forEach(element => {
+                ingredientModal.innerHTML += `${element.name}`;
+                ingredientModal.innerHTML += `${element.amount}`;
+            }));
+        });
+        recettes.appendChild(button);
+    });
 }
-function chargerListeIngredients(element) {
-    let option = document.createElement('option');
-    option.innerHTML = `${ingredient[0][0].name}`;
-    ingredientsIndex.appendChild(option);
-}
-console.table(tabRecipes);
-console.log(tabRecipes[0].name);
+;
+console.table(selectedRecipe);
+//Affichage ingrédient dans la liste
+tabRecipes.forEach(element => {
+    const ingredientsList = element.ingredients;
+    console.log(ingredientsList.forEach(element => {
+        let option = document.createElement('option');
+        option.innerHTML = `${element.name}`;
+        ingredientsIndex.appendChild(option);
+    }));
+});
--- TPCuisine/filter_recipes/main.ts
+++ TPCuisine/filter_recipes/main.ts
@@ -7,9 +7,10 @@ let search = document.getElementById("search") as HTMLDivElement
 let ingredientsIndex = document.getElementById("ingredients") as HTMLSelectElement
 let filter = document.getElementById("filter") as HTMLButtonElement
 let recettes = document.getElementById("recettes") as HTMLDivElement
-
-
-
+let modal = document.getElementById("modal") as HTMLDivElement
+let titre = document.getElementById("titre") as HTMLDivElement
+let ingredientModal = document.getElementById("ingredientModal") as HTMLDivElement
+let instructionModal = document.getElementById("instruction") as HTMLDivElement
 
 interface Recipe {
     id?: string;
@@ -18,74 +19,79 @@ interface Recipe {
     prepTime: string;
     cookTime: string;
     ingredients: {
-      name: string;
-      amount: string;
+        name: string;
+        amount: string;
     }[];
     instructions: string[];
-  }
+}
 
 let tabRecipes: Recipe[] = []
+let selectedRecipe = tabRecipes[0];
 
 
-    for (const key  in recipes) {
-        if (recipes.hasOwnProperty.call(recipes, key)) {
-            const element = recipes[key];
-            tabRecipes.push(element)
-
-          
-        }
+for (const key in recipes) {
+    if (recipes.hasOwnProperty.call(recipes, key)) {
+        const element = recipes[key];
+        tabRecipes.push(element)
     }
-  
-    const ingredient = tabRecipes.map(el => el.ingredients)
-  
-    console.log(ingredient[0][0].name);
-
-/*     console.log(tabRecipes[0].name);
-tabRecipes.forEach(element => {
-    for (let i=0; i<ingredients.length; i++) {
-        console.log(`${element.ingredients[i]}`);
-            }
-}); */
-
-       let i =0
-            window.addEventListener('load', function() {
-                
-                tabRecipes.forEach(element => {
-                  createBouton(element)
-               
-                  chargerListeIngredients(element)
-
-                });
-            })
-
-
-    function createBouton (element: Recipe) {
-        let button = document.createElement('button')
-        button.type = 'button';
-        button.innerHTML=`${element.name} <hr>` 
-        button.innerHTML +=`<i class="bi bi-egg-fried"></i>${element.cookTime}  <i class="bi bi-fire"></i>${element.prepTime} ` 
-        button.className = "btn btn-outline-light btn-lg btn-block text-center"
-        recettes.appendChild(button);
-
-        
-    }
-function chargerListeIngredients (element: Recipe ) {
-let option = document.createElement('option')
-option.innerHTML = `${ingredient[0][0].name }` 
-ingredientsIndex.appendChild(option)
 }
-    
-
-  
-
-
-   
-   
-   
-
-
- 
 
 
 console.table(tabRecipes);
-console.log(tabRecipes[0].name);
+
+
+window.addEventListener('load', function () {
+    console.log("object");
+    tabRecipes.forEach(element => {
+        createBouton(element)
+       
+
+    });
+})
+
+function createBouton (element: Recipe) {
+    let button = document.createElement('button')
+    button.type = 'button';
+    button.innerHTML=`${element.name} <hr>` 
+    button.className = element === selectedRecipe ? "btn btn-light w-100 my-2" : "btn btn-outline-light w-100 my-2";
+    button.innerHTML +=`<i class="bi bi-egg-fried"></i>${element.cookTime}  <i class="bi bi-fire"></i>${element.prepTime} ` 
+    button.addEventListener('click', ()=> {
+        selectedRecipe= element;
+        console.log(selectedRecipe);
+        modal.style.display = "block"
+        titre.innerHTML = element.name
+        instructionModal.innerHTML = `${element.instructions}`
+      
+    })
+    recettes.appendChild(button);
+
+    button.addEventListener('click', () => {
+        selectedRecipe = element
+        console.log(selectedRecipe);
+     
+      
+        
+        tabRecipes.forEach(element => {
+            const ingredientsList = element.ingredients;
+            console.log(ingredientsList.forEach(element => {
+                ingredientModal.innerHTML += `${element.name}`
+                ingredientModal.innerHTML += `${element.amount}`
+            }))
+        })
+        recettes.appendChild(button);
+    })
+};
+
+
+console.table(selectedRecipe);
+
+//Affichage ingrédient dans la liste
+tabRecipes.forEach(element => {
+    const ingredientsList = element.ingredients;
+    console.log(ingredientsList.forEach(element => {
+        let option = document.createElement('option')
+        option.innerHTML = `${element.name}`
+        ingredientsIndex.appendChild(option)
+    }));
+
+});

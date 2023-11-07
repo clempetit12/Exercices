d6b4d0a7cd33f7652ba15749aedf18cd8879c4fe
clempetit12 <143411906+clempetit12@users.noreply.github.com>
2023-11-07 14:15:45 +0100

    VF description articles

--- React Native/Exercice liste/List/Components/ModalDetails.js
+++ React Native/Exercice liste/List/Components/ModalDetails.js
@@ -12,16 +12,45 @@ export default function ModalDetails({ article, closeModalDetails }) {
 
     }
     return (
-        <Modal>
-            <Text>DESCRIPTION ARTICLE</Text>
-            <Text> Nom article : {article.text}</Text>
-            <Text> Prix article : {article.price}</Text>
-            <Text> Description article : {article.description}</Text>
-            <Button onPress={() => deleteArticle(article.id)} color="red" title="Supprimer"></Button>
-            <Button onPress={() => closeModalDetails()} color="blue" title="Articles"></Button>
+        <Modal style={styles.container}>
+            <View style={styles.centeredContainer}>
+                <Text style={styles.boldText}>DESCRIPTION ARTICLE</Text>
+                <Text> Nom article : {article.text}</Text>
+                <Text> Prix article : {article.price}</Text>
+                <Text> Description article : {article.description}</Text>
+                <View style={styles.buttons}>
+                    <Button onPress={() => deleteArticle(article.id)} color="red" title="Supprimer"></Button>
+                    <Button onPress={() => closeModalDetails()} color="blue" title="Articles"></Button>
+                </View>
+
+            </View>
+
+
         </Modal>
 
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        marginTop: 20,
+    },
+    centeredContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    boldText: {
+        fontSize: 20,
+        fontWeight: '900',
+        color: 'black',
+    },
+    buttons: {
+        flexDirection: "row",
+        justifyContent: 'space-around',
+        width: '70%',
+        marginTop: 20
+    },
+})

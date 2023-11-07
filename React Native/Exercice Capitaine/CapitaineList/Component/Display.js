4e7c264757aa064501eaa6101c85b7ed9b24f7e6
clempetit12 <143411906+clempetit12@users.noreply.github.com>
2023-11-07 09:50:03 +0100

    VF

--- React Native/Exercice Capitaine/CapitaineList/Component/Display.js
+++ React Native/Exercice Capitaine/CapitaineList/Component/Display.js
@@ -7,18 +7,20 @@ export default function Display() {
     return (
         <View style={styles.container}>
             <View style={styles.container2}>
-            <Image style={styles.image}
-                width={100}
-                height={100}
-                source={{
-                    uri: "https://cdn.buttercms.com/O7wz509NSOOyixCnLn1x"
-                }} />
+                <Image style={styles.image}
+                    width={100}
+                    height={100}
+                    source={{
+                        uri: "https://cdn.buttercms.com/O7wz509NSOOyixCnLn1x"
+                    }} />
             </View>
-          
-            <Text>Nom: <Text style={styles.textBold}>Capitaine</Text></Text>
-            <Text>Prenom: <Text style={styles.textBold}>Haddock</Text> </Text>
-            <Text>Téléphone: <Text style={styles.textBold}>01.23.36.48.75</Text></Text>
-            <Text>Adresse: <Text style={styles.textBold}>35 rue des pias 59200 Tourcoing</Text> </Text>
+         
+                <Text style={styles.textStyle}>Nom: <Text style={styles.textBold}>Capitaine</Text></Text>
+                <Text style={styles.textStyle}>Prenom: <Text style={styles.textBold}>Haddock</Text> </Text>
+                <Text style={styles.textStyle}>Téléphone: <Text style={styles.textBold}>01.23.36.48.75</Text></Text>
+                <Text style={styles.textStyle}>Adresse: <Text style={styles.textBold}>35 rue des pias 59200 Tourcoing</Text> </Text>
+            
+
         </View>
 
     )
@@ -28,20 +30,24 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
-        backgroundColor: "beige",
-        
+        backgroundColor: "#faebd6",
+
         padding: 20,
         marginTop: 20,
     },
-    container2 : {
+    container2: {
         justifyContent: "center",
         alignItems: "center",
     },
+    textStyle: {
+        fontSize: 20
+    },
     textBold: {
-        fontWeight: '800',
+        fontWeight: '900',
+        color: 'black'
     },
     image: {
-    
+
         borderRadius: 100,
         marginBottom: 20
     }
